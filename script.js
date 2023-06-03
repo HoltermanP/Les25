497e67d9bd032762f70b574f9dd21933df6a8a1d
HoltermanP <118934355+HoltermanP@users.noreply.github.com>
2023-06-03 22:41:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("#hideButton").click(function() {
+      $("#hiddenContent").hide();
+    });
+  
+    $("#showButton").click(function() {
+      $("#hiddenContent").show();
+    });
+  });
+  
